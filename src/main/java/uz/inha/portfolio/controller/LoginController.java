3c5c104e44344ca89e72d0022a37d6553ca4f376
Jabborov Egamberdi <jabborovegamberdi2002@gmail.com>
2022-05-31 10:32:21 +0500

    LoginController added. UserController updated

---
+++ src/main/java/uz/inha/portfolio/controller/LoginController.java
@@ -0,0 +1,34 @@
+package uz.inha.portfolio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import uz.inha.portfolio.dto.UserDto;
+import uz.inha.portfolio.model.Users;
+import uz.inha.portfolio.service.UserService;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_page";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@ModelAttribute UserDto userDto) {
+        Users users = userService.hasPhone(userDto.getPhone());
+        if (users == null || !users.getPassword().equals(userDto.getPassword())) {
+            return "index";
+        }
+        if (users.getIsAdmin()) {
+            return "admin_page";
+        }
+        return "user_page";
+    }
+}
